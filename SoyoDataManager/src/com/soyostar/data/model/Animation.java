1c24c0fd3d07e303656a478a7d0787debdece7b2
uestccokey <uestccokey@gmail.com>
2011-04-28 12:50:40 +0000

    cokey4.28

---
+++ SoyoDataManager/src/com/soyostar/data/model/Animation.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soyostar.data.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Animation extends Model {
+
+    public ArrayList<TileSet> tileSets = new ArrayList<TileSet>();
+    public ArrayList<Sequence> sequences = new ArrayList<Sequence>();
+}
